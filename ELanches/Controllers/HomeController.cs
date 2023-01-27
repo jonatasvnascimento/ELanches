99770967d487a6954324a07be68c4e350dcf5a11
Jonatas Nascimento <jonatas.nascimento@elisbrasil.onmicrosoft.com>
2023-01-27 20:04:14 -0300

    Add project files.

---
+++ ELanches/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using ELanches.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ELanches.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
