e7076994527216e0933dbc84acb4d180669d7fb5
Jonatas Nascimento <jonatas.nascimento@elisbrasil.onmicrosoft.com>
2023-01-30 09:39:00 -0300

    14 funcionamento do projeto

--- ELanches/Controllers/HomeController.cs
+++ ELanches/Controllers/HomeController.cs
@@ -11,6 +11,11 @@ namespace ELanches.Controllers
             return View();
         }
 
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
