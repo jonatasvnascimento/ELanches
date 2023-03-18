c3492c6fff765a2e1db32e3e6bb1d5165e12a106
Jonatas Nascimento <56093381+jonatasvnascimento@users.noreply.github.com>
2023-03-18 08:52:13 -0300

    dev: finalizando criação de interface ILanchesRepository e classe LanchesRepository

---
+++ ELanches/Repositories/Interfaces/ILanchesRepository.cs
@@ -0,0 +1,11 @@
+﻿using ELanches.Models;
+
+namespace ELanches.Repositories.Interfaces
+{
+    public interface ILanchesRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPreferidos { get; }
+        Lanche GetLancheById(int lanchesId);
+    }
+}
---
+++ ELanches/Repositories/LanchesRepository.cs
@@ -0,0 +1,25 @@
+﻿using ELanches.Context;
+using ELanches.Models;
+using ELanches.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELanches.Repositories
+{
+    public class LanchesRepository : ILanchesRepository
+    {
+        private readonly AppDbContext _context;
+        public LanchesRepository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches.Where(c => c.IsLanchePreferido).Include(c => c.Categoria);
+
+        public Lanche GetLancheById(int lanchesId)
+        {
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == lanchesId);
+        }
+    }
+}
