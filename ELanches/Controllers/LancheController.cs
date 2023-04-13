eae869c82a9077b431810d243ed5b812c9fe03a4
Jonatas Nascimento <56093381+jonatasvnascimento@users.noreply.github.com>
2023-04-13 13:47:46 -0300

    dev: adicionado controller e List para visualizar lanches

---
+++ ELanches/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ELanches.Repositories.Interfaces;
+
+namespace ELanches.Controllers
+{
+    public class LancheController : Controller
+    {
+        public readonly ILanchesRepository _ILanchesRepository;
+
+        public LancheController(ILanchesRepository lanchesRepository)
+        {
+            _ILanchesRepository = lanchesRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _ILanchesRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
