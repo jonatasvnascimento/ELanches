381f08ee00a404f7a834aad1c13d500aff7c5819
Jonatas Nascimento <56093381+jonatasvnascimento@users.noreply.github.com>
2023-08-24 20:45:52 -0300

    feat: adicionando metodos GetCarrinhoCompraItens, LimparCarrinho, GetCarrinhoCompraTotal do carrinho

--- ELanches/Models/CarrinhoCompra.cs
+++ ELanches/Models/CarrinhoCompra.cs
@@ -1,4 +1,5 @@
 ﻿using ELanches.Context;
+using Microsoft.EntityFrameworkCore;
 using System.Security.AccessControl;
 
 namespace ELanches.Models
@@ -72,6 +73,31 @@ namespace ELanches.Models
             }
             _context.SaveChanges();
             return quantidadeLocal;
+        }
+
+        public List<CarrinhoCompraItem> GetCarrinhoCompraItens()
+        {
+            return CarrinhoCompraItens ?? (CarrinhoCompraItens = _context.CarrinhoCompraItens
+                .Where(c => c.CarrinhoCompraId == CarrinhoCompraId)
+                .Include(s => s.Lanche)
+                .ToList());
+        }
+        public void LimparCarrinho()
+        {
+            var carrinhoItens = _context.CarrinhoCompraItens
+                .Where(carrinho => carrinho.CarrinhoCompraId == CarrinhoCompraId);
+
+            _context.CarrinhoCompraItens.RemoveRange(carrinhoItens);
+            _context.SaveChanges();
+        }
+
+        public decimal GetCarrinhoCompraTotal()
+        {
+            var total = _context.CarrinhoCompraItens
+                .Where(c => c.CarrinhoCompraId == CarrinhoCompraId)
+                .Select(c => c.Lanche.Preco * c.Quantidade)
+                .Sum();
+            return total;
         }
     }
 }
