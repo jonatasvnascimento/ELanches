75f19d8577b47f0511cdfb98509e61bf043b0fd7
Jonatas Nascimento <56093381+jonatasvnascimento@users.noreply.github.com>
2023-10-17 22:08:47 -0300

    feat: implementação de dropdown e com filtro passando os parametros para controller Lanches/List

---
+++ ELanches/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using ELanches.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELanches.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        public readonly ICategoriaRepository _ICategoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository iCategoriaRepository)
+        {
+            _ICategoriaRepository = iCategoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _ICategoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
