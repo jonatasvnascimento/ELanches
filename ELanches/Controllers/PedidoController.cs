c036a7d99e84deeca7e1dc5baccde7d271d77eb3
Jonatas Nascimento <56093381+jonatasvnascimento@users.noreply.github.com>
2024-01-29 20:22:40 -0300

    feat: Criando o controlador PedidoController

---
+++ ELanches/Controllers/PedidoController.cs
@@ -0,0 +1,45 @@
+﻿using ELanches.Models;
+using ELanches.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELanches.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            if (_carrinhoCompra.CarrinhoCompraItens.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio, inclua um lanche...");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+                _carrinhoCompra.LimparCarrinho();
+                return RedirectToAction("CheckoutCompleto");
+            }
+
+            return View(pedido);
+        }
+    }
+}
